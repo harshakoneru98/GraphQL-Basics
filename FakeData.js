da7d1db0cc1f974111de2e901f1f7977c16d6e9d
Harsha Vardhan Koneru <hkoneru@usc.edu>
2022-02-24 21:47:15 -0800

    Basic Configuration

---
+++ FakeData.js
@@ -0,0 +1,39 @@
+const UserList = [
+    {
+        id: 1,
+        name: 'Harsha',
+        username: 'khv2077',
+        age: 23,
+        nationality: 'United States'
+    },
+    {
+        id: 2,
+        name: 'Vamsi',
+        username: 'kvk2022',
+        age: 25,
+        nationality: 'Canada'
+    },
+    {
+        id: 3,
+        name: 'Nandu',
+        username: 'deepak2020',
+        age: 24,
+        nationality: 'Australia'
+    },
+    {
+        id: 4,
+        name: 'Hema',
+        username: 'hema2017',
+        age: 22,
+        nationality: 'India'
+    },
+    {
+        id: 5,
+        name: 'Samba Siva Rao',
+        username: 'kssr1966',
+        age: 56,
+        nationality: 'Vietnam'
+    }
+];
+
+module.exports = { UserList };
